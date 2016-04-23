3c39e690a8ca99e9204b03a05f89e7c4fad3cede
Arun-Patil <arunkumar@udbhava.co.in>
2016-04-23 11:34:53 +0530

    Test ok on Local
    Successfully done

---
+++ app/app.js
@@ -0,0 +1,22 @@
+(function(){
+	
+	"use strict";
+
+	angular.module('farmerExe', ['ngRoute']);
+	/*.config(['$routeProvider',
+	  function($routeProvider) {
+	    $routeProvider.
+	      when('/', {
+	        //templateUrl: '',
+	        //controller: ''
+	      }).
+	      when('/', {
+	        //templateUrl: '',
+	        //controller: ''
+	      }).
+	      otherwise({
+	       //redirectTo: '/'
+	      });
+	}]);*/
+
+})();
